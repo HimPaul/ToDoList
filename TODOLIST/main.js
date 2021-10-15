77837fe2108d70f253681dba1538bf63c7184431
HimPaul <46809310+HimPaul@users.noreply.github.com>
2021-10-15 03:35:05 -0700

    Add files via upload
    TODO LIST USING JAVASCRIPT ...ADD + BUTTON TO ADD ELEMENTS AND DOUBLECLICK ON ELEMENTS TO REMOVE THE ELEMENTS

---
+++ TODOLIST/main.js
@@ -0,0 +1,15 @@
+
+let addtodobutton = document.getElementById('addtodo');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('InputField');
+
+addtodobutton.addEventListener('click',function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText = inputfield.value;
+    todocontainer.appendChild(paragraph)
+    inputfield.value ="";
+    paragraph.addEventListener('dblclick',function(){
+        todocontainer.removeChild(paragraph);
+
+    })
+})
